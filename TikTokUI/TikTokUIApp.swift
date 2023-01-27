1faefea57b8ade1493848306109d4683464c4d56
Djallil Elkebir <elkebir.djallil@icloud.com>
2023-01-27 13:47:49 -0500

    Initial Commit

---
+++ TikTokUI/TikTokUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokUIApp.swift
+//  TikTokUI
+//
+//  Created by Djallil on 2023-01-27.
+//
+
+import SwiftUI
+
+@main
+struct TikTokUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
